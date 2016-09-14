c7cb90fb274f0e07c607e985ee83b05a024114d0
Harutyun Sadoyan <hsadoyan@CCCWORK4.WPI.EDU>
2016-09-14 13:59:18 -0400

    Minor Fixes

--- c_server.c
+++ c_server.c
@@ -80,7 +80,7 @@ int main(int argc, char* argv[])
         memset(file_buffer, 0, FILE_BUFFER_SIZE);
 
 
-        char* file_name = "t_file.txt";
+        char* file_name = "testdir/t_file.txt";
 
         // Infinite Loop
         // Those are bad, except when they're not
